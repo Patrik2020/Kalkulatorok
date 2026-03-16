c99f5b3c1eb226cf1c1a0c87207c90b26842ed14
Kovács Patrik <65179610+Patrik2020@users.noreply.github.com>
2026-03-16 17:58:01 +0100

    Add files via upload

---
+++ js/kalkulator.js
@@ -0,0 +1,23 @@
+function calculateInflation() {
+  let money = Number(document.getElementById("money").value);
+  let inflation = Number(document.getElementById("inflation").value) / 100;
+  let years = Number(document.getElementById("years").value);
+  let result = money / Math.pow(1 + inflation, years);
+
+  document.getElementById("result-value").innerText =
+    Math.round(result).toLocaleString("hu-HU") + " Ft";
+}
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("input", calculateInflation);
+});
+
+function calculateCompoundInterest() {
+  let principal = Number(document.getElementById("principal").value);
+  let rate = Number(document.getElementById("rate").value) / 100;
+  let years = Number(document.getElementById("years").value);
+  let result = principal * Math.pow(1 + rate, years);
+
+  document.getElementById("result-value").innerText =
+    Math.round(result).toLocaleString("hu-HU") + " Ft";
+}
