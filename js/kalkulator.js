3a7d29df348e78893d400cbcfbd72f81be53b883
Kovács Patrik <65179610+Patrik2020@users.noreply.github.com>
2026-03-17 14:38:10 +0100

    Add files via upload

--- js/kalkulator.js
+++ js/kalkulator.js
@@ -39,3 +39,31 @@ function goBack() {
     window.location.href = "../index.html";
   }
 }
+function calculateConcrete() {
+  let length = Number(document.getElementById("length").value);
+  let width = Number(document.getElementById("width").value);
+  let height = Number(document.getElementById("height").value) / 100;
+
+  let volume = length * width * height;
+
+  document.getElementById("result-value").innerText = volume.toFixed(2) + " m³";
+}
+
+function calculateTiles() {
+  let roomLength = Number(document.getElementById("roomLength").value);
+  let roomWidth = Number(document.getElementById("roomWidth").value);
+
+  let tileLength = Number(document.getElementById("tileLength").value) / 100;
+  let tileWidth = Number(document.getElementById("tileWidth").value) / 100;
+
+  let waste = Number(document.getElementById("waste").value) / 100;
+
+  let roomArea = roomLength * roomWidth;
+  let tileArea = tileLength * tileWidth;
+
+  let tiles = roomArea / tileArea;
+
+  let total = tiles * (1 + waste);
+
+  document.getElementById("result-value").innerText = Math.ceil(total) + " db";
+}
